05a5685248c33409fc8f21a1ec1570bf497be2f8
Dan Bickford <danbickford007@yahoo.com>
2018-04-27 21:08:55 -0400

    Getting binary working

--- lib/superstash.rb
+++ lib/superstash.rb
@@ -1,4 +1,15 @@
 require "superstash/version"
 
 module Superstash
+  def hello
+    puts "-" * 99
+    puts "SuperStashing..."
+    puts "-" * 99
+  end
+
+  def add_message
+    puts 'Add a message to identify this stash'
+    message = gets
+    File.open("#{File.expand_path File.dirname(__FILE__)}/stashes/mystash", 'w') { |file| file.write(message) }
+  end
 end
