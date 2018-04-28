dbc11dd3931cc38d0ffda1db9d40d45941fdf09d
Dan Bickford <danbickford007@yahoo.com>
2018-04-27 21:21:03 -0400

    Starting to actually stash

--- lib/superstash.rb
+++ lib/superstash.rb
@@ -1,15 +1,27 @@
 require "superstash/version"
 
 module Superstash
+
+  PATH = File.expand_path File.dirname(__FILE__)
+
   def hello
+    create_stashes_if_missing
     puts "-" * 99
     puts "SuperStashing..."
     puts "-" * 99
   end
 
+  def create_stashes_if_missing
+    if !Dir.exist? "#{PATH}/stashes"
+      Dir.mkdir "#{PATH}/stashes"
+    end
+  end
+
   def add_message
     puts 'Add a message to identify this stash'
     message = gets
-    File.open("#{File.expand_path File.dirname(__FILE__)}/stashes/mystash", 'w') { |file| file.write(message) }
+    list = Dir.entries "#{PATH}/stashes"
+    Dir.mkdir "#{PATH}/stashes/stash-#{list.count}"
+    File.open("#{PATH}/stashes/stash-#{list.count}/message", 'w') { |file| file.write(message) }
   end
 end
